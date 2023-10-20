c175bdc3a8751801eebc9a8b7ce79eb445c5ac90
Michele Di Corato <micheled.46@hotmail.it>
2023-10-20 13:14:44 +0200

    Creation edit form

--- src/app/movieTab/movieEditTab/movieEditTab.page.ts
+++ src/app/movieTab/movieEditTab/movieEditTab.page.ts
@@ -2,20 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/shared/services/movies.service';
 import { MovieList } from 'src/app/shared/interfaces/movies.interface';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-movie-edit',
   templateUrl: 'movieEditTab.page.html',
   styleUrls: ['movieEditTab.page.scss'],
 })
-export class MovieEditTabPage implements OnInit {
+export class MovieEditTabPage {
   movie: MovieList | undefined;
+  formUser: FormGroup | undefined;
   constructor(
     private readonly _movieService: MovieService,
-    private readonly _route: ActivatedRoute
-  ) {}
-  ngOnInit(): void {
+    private readonly _route: ActivatedRoute,
+    private readonly _location: Location
+  ) {
     const id = this._route.snapshot.params['id'];
     this.movie = this._movieService.getMovieById(Number(id));
+    this._setForm();
+  }
+
+  private _setForm() {
+    this.formUser = new FormGroup({
+      id: new FormControl(this.movie?.id),
+      title: new FormControl(this.movie?.title, Validators.minLength(5)),
+      director: new FormControl(this.movie?.director, Validators.minLength(5)),
+      releaseYear: new FormControl(this.movie?.releaseYear),
+      description: new FormControl(
+        this.movie?.description,
+        Validators.maxLength(1000)
+      ),
+      genres: new FormControl(this.movie?.genres),
+      celebrities: new FormControl(this.movie?.celebrities),
+      countries: new FormControl(this.movie?.countries),
+      rating: new FormControl(this.movie?.rating),
+    });
+  }
+
+  submitForm() {
+    if (this.formUser?.valid) {
+      //this._movieService.update(this.formUser?.value).subscribe(() => )
+      this._location.back();
+    }
   }
 }
--- src/app/movieTab/movieTab.module.ts
+++ src/app/movieTab/movieTab.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MovieTabPage } from './movieTab.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -9,12 +9,12 @@ import { MovieTabPageRoutingModule } from './movieTab-routing.module';
 import { ListComponent } from '../shared/components/list/list.component';
 import { MovieDetailTabPage } from './movieDetailTab/movieDetailTab.page';
 import { MovieEditTabPage } from './movieEditTab/movieEditTab.page';
-
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ExploreContainerComponentModule,
     MovieTabPageRoutingModule,
   ],
