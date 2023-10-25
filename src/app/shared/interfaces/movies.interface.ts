5574f68e16125a175904b3f71b12ac883ff8d8c5
Michele Di Corato <micheled.46@hotmail.it>
2023-10-25 10:10:03 +0200

    Updated detail view

--- src/app/shared/interfaces/movies.interface.ts
+++ src/app/shared/interfaces/movies.interface.ts
@@ -4,4 +4,27 @@ export interface MovieList {
   year: number;
   runningTime: number;
   genres?: string;
+  cast?: Cast[];
+  rating: Rating;
+  country?: Country[];
+}
+
+export interface Cast {
+  celebrityId: string;
+  celebrityName: string;
+  movieId: string;
+  movieTitle: string;
+  category: string;
+  characters?: string;
+}
+
+export interface Rating {
+  averageRating?: number;
+  numVotes?: number;
+}
+
+export interface Country {
+  title: string;
+  region: string;
+  language?: string;
 }
